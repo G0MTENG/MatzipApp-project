35472a588690943fb59880dbd0ef4e22512886d0
g0mteng <gomteng03@naver.com>
2024-11-25 13:19:35 +0900

    alias import 에러 수정

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -7,7 +7,7 @@
 
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
-import RootNavigator from './src/navigations/root/RootNavigator';
+import {RootNavigator} from './src/navigations/root/RootNavigator';
 
 function App(): JSX.Element {
   return (
